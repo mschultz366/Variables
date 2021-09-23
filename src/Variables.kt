41963a8a08a4e6719559b093b6868a37c86b55d6
mschultz366 <mschultz366@students.mchenry.edu>
2021-09-23 14:34:10 -0500

    Initial commit

---
+++ src/Variables.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+// program lists and names variables
+    var myByte: Byte = 2
+    var myShort: Short = 4
+    var myInt: Int = 8
+    var myLong: Long = 999999L
+    var myFloat: Float = 44.44F
+    var myDouble: Double = 323.454
+    var myBoolean: Boolean = true
+    var myChar: Char = 'A'
+
+    println("Byte: $myByte")
+    println("Short: $myShort")
+    println("Int: $myInt")
+    println("Long: $myLong")
+    println("Float: $myFloat")
+    println("Double: $myDouble")
+    println("Boolean: $myBoolean")
+    println("Char: $myChar")
+}
